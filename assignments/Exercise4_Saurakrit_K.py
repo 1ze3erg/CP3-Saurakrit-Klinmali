b5febc33c1772c67aa359cab0f56f898d7a6447d
1ze3erg <skizeza666@gmail.com>
2021-01-13 19:39:40 +0700

    Exercise4_Saurakrit_K.py

---
+++ assignments/Exercise4_Saurakrit_K.py
@@ -0,0 +1,17 @@
+"""
+Exercise4
+FE = Foundation English
+GB = General Business
+CS = Computer Systems
+CP = Computer Programming
+"""
+FE_Score = 80
+GB_Score = 66.5
+introCS_Score = 50
+CP_Score = 99.99
+print("------- Your Score -------")
+print("Foundation English:", FE_Score)
+print("General Business:", GB_Score)
+print("Introduction to Computer Systems:", introCS_Score)
+print("Computer Programming:", CP_Score)
+print("--------------------------")
