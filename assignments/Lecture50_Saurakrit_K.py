89d6c4a9990fa0966514842b25a067112a4d177c
1ze3erg <skizeza666@gmail.com>
2021-01-13 19:41:35 +0700

    Lecture50_Saurakrit_K.py

---
+++ assignments/Lecture50_Saurakrit_K.py
@@ -0,0 +1,8 @@
+def calculateNumber(x,y):
+    print("x + y =",x+y)
+    print("x - y =",x-y)
+    print("x * y =",x*y)
+    print("x / y =",x/y)
+x = int(input("x: "))
+y = int(input("y: "))
+calculateNumber(x,y)
