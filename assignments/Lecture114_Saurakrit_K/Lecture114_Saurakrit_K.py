aff1cdbf4eaa07e27a2691cecfa5827e47b0522a
1ze3erg <skizeza666@gmail.com>
2021-01-18 19:15:59 +0700

    fix import error

--- assignments/Lecture114_Saurakrit_K/Lecture114_Saurakrit_K.py
+++ assignments/Lecture114_Saurakrit_K/Lecture114_Saurakrit_K.py
@@ -1,8 +1,7 @@
-from tkinter import *
-from tkinter import ttk
-from forex_python.converter import *
 import tkinter as tk
-from tkcalendar import *
+from tkinter import ttk
+from forex_python.converter import CurrencyRates
+from tkcalendar import Calendar
 import datetime
 
 cur_rate = CurrencyRates()
@@ -66,14 +65,14 @@ main_windows.geometry('300x700')
 
 
 # money1
-money1_label = Label(main_windows, text="money1")
+money1_label = tk.Label(main_windows, text="money1")
 money1_label.pack(expand=True)
-money1_entry = Entry(main_windows)
+money1_entry = tk.Entry(main_windows)
 money1_entry.pack(expand=True)
 
 
 # สกุลเงิน1
-cur1_label = Label(main_windows, text="currency1")
+cur1_label = tk.Label(main_windows, text="currency1")
 cur1_label.pack(expand=True)
 cur1_entry = ttk.Combobox(main_windows,
                           values=['AUD',
@@ -113,24 +112,24 @@ cur1_entry.pack(expand=True)
 
 
 # ให้เลือกว่าจะแปลงขึ้นหรือลง
-var = IntVar()
-filename1 = PhotoImage(file='arrow-up.png').subsample(x=70, y=70)
-filename2 = PhotoImage(file='arrow-down.png').subsample(x=70, y=70)
-R1 = Radiobutton(main_windows, variable=var, value=1, image=filename1)
+var = tk.IntVar()
+filename1 = tk.PhotoImage(file='arrow-up.png').subsample(x=70, y=70)
+filename2 = tk.PhotoImage(file='arrow-down.png').subsample(x=70, y=70)
+R1 = tk.Radiobutton(main_windows, variable=var, value=1, image=filename1)
 R1.pack()
-R2 = Radiobutton(main_windows, variable=var, value=2, image=filename2)
+R2 = tk.Radiobutton(main_windows, variable=var, value=2, image=filename2)
 R2.pack()
 
 
 # money2
-money2_label = Label(main_windows, text="money2")
+money2_label = tk.Label(main_windows, text="money2")
 money2_label.pack(expand=True)
-money2_entry = Entry(main_windows)
+money2_entry = tk.Entry(main_windows)
 money2_entry.pack(expand=True)
 
 
 # สกุลเงิน2
-cur2_label = Label(main_windows, text="currency2")
+cur2_label = tk.Label(main_windows, text="currency2")
 cur2_label.pack(expand=True)
 cur2_entry = ttk.Combobox(main_windows,
                           values=['AUD',
@@ -170,19 +169,19 @@ cur2_entry.pack(expand=True)
 
 
 # ปุ่มคำนวณ
-convert_button = Button(main_windows, text="แปลงค่าเงิน",
+convert_button = tk.Button(main_windows, text="แปลงค่าเงิน",
                         width=20, bg="grey", command=convert_currency)
 convert_button.pack(fill=tk.Y, expand=True)
 
 
 # ปุ่ม clear จำนวนเงิน
-clear_button = Button(main_windows, text="clear money",
+clear_button = tk.Button(main_windows, text="clear money",
                       width=20, bg="red", command=clear_money)
 clear_button.pack(fill=tk.Y, expand=True, pady=5)
 
 # ช่องแสดงวันที่และปุ่มclearวันที่
 calendar_date_pattern = tk.Label(main_windows, text="Date(y-mm-dd)", bg='cyan')
-calendar_date_pattern.pack(fill=BOTH, expand=True)
+calendar_date_pattern.pack(fill='both', expand=True)
 calendar_label = tk.Label(main_windows, text="", bg='cyan')
 calendar_label.pack(fill=tk.Y, expand=True)
 calendar_clear_date = tk.Button(
@@ -195,6 +194,6 @@ maxdate = datetime.date.today()
 calendar = Calendar(main_windows, selectmode="day", date_pattern='y-mm-dd',
                     firstweekday='sunday', disableddaybackground='grey', cursor="hand2",
                     mindate=mindate, maxdate=maxdate)
-calendar.pack(side=RIGHT, expand=True, pady=5)
+calendar.pack(side='right', expand=True, pady=5)
 
 main_windows.mainloop()
